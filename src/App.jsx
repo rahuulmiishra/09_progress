6c928c85af389ab682fe7ae286b0b70a084ce249
Rahul Mishra <rahulmishra21791@gmail.com>
2024-09-21 09:16:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import Progress from "./progress";
+
+function App() {
+  const [val, setVal] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setVal((prev) => prev + 10);
+    }, 2000);
+  }, []);
+
+  function handleStart() {
+    console.log("Started");
+  }
+
+  function handleComplete() {
+    console.log("Completed");
+  }
+
+  return (
+    <Progress onComplete={handleComplete} onStart={handleStart} value={val} />
+  );
+}
+
+export default App;
